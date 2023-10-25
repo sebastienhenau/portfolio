462b42fea735590a39b2163fe6c9653fd2cfc92c
Sébastien Henau <sebastienhenau@hotmail.com>
2023-10-25 09:40:17 +0200

    feat: add colors for light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		colors: {
-			background: "hsl(var(--color-background) / <alpha-value>)",
+			site: {
+				1: "hsl(var(--color-site-1) / <alpha-value>)",
+				2: "hsl(var(--color-site-2) / <alpha-value>)",
+			},
+			text: {
+				1: "hsl(var(--color-text-1) / <alpha-value>)",
+				2: "hsl(var(--color-text-2) / <alpha-value>)",
+				3: "hsl(var(--color-text-3) / <alpha-value>)",
+			},
+			accent: "hsl(var(--color-accent) / <alpha-value>)",
+			border: "hsl(var(--color-border) / <alpha-value>)",
+			shadow: "hsl(var(--color-border))",
 		},
 	},
 	plugins: [],
