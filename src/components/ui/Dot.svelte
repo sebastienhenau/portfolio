f9b56d9b1e268f34172b9ff64ad04732f89730af
Sébastien Henau <sebastienhenau@hotmail.com>
2024-02-12 13:30:46 +0100

    fix: dot border color in dark mode

--- src/components/ui/Dot.svelte
+++ src/components/ui/Dot.svelte
@@ -4,7 +4,7 @@
 
 <div
     class={clsx(
-        'w-[0.625rem] h-[0.625rem] border border-border rounded-full bg-site-base point-events-none',
+        'w-[0.625rem] h-[0.625rem] border border-line rounded-full bg-site-base point-events-none',
         $$props.class
     )}
 />
