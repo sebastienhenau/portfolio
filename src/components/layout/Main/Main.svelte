7eda7fa9acf7feb96a9580e88292f19c26a6536c
Sébastien Henau <sebastienhenau@hotmail.com>
2024-02-12 13:29:11 +0100

    feat: hide dots in main on mobile

--- src/components/layout/Main/Main.svelte
+++ src/components/layout/Main/Main.svelte
@@ -6,9 +6,9 @@
 <main class={clsx('relative 900:pl-7', $$props.class)}>
     <div class="max-900:hidden absolute top-0 left-0 w-border h-full bg-gradient-vertical-dashed -translate-x-1/2" />
 
-    <Dot class="absolute top-0 left-0 -translate-x-1/2" />
+    <Dot class="max-900:hidden absolute top-0 left-0 -translate-x-1/2" />
 
     <slot />
 
-    <Dot class="absolute bottom-0 left-0 -translate-x-1/2" />
+    <Dot class="max-900:hidden absolute bottom-0 left-0 -translate-x-1/2" />
 </main>
