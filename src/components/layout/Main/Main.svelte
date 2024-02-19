adef676723ba45a92e6a0b6107071463c3b943c2
Sébastien Henau <sebastienhenau@hotmail.com>
2024-02-19 16:34:55 +0100

    feat: remove dot in main component

--- src/components/layout/Main/Main.svelte
+++ src/components/layout/Main/Main.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
     import clsx from 'clsx';
-    import { Dot } from '$components';
 </script>
 
 <main class={clsx('relative 900:pl-7', $$props.class)}>
     <div class="max-900:hidden absolute top-0 left-0 w-border h-full bg-gradient-vertical-dashed -translate-x-1/2" />
 
-    <Dot class="max-900:hidden absolute top-0 left-0 -translate-x-1/2" />
-
     <slot />
-
-    <Dot class="max-900:hidden absolute bottom-0 left-0 -translate-x-1/2" />
 </main>
